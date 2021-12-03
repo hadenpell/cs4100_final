8bffb78a871694fd5bc0695d0f6dbf764113ad62
Dsapi5131 <dsap@tutamail.com>
2021-12-03 12:59:02 -0500

    Update with completed random main.py

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 import mlagents
+
 from mlagents_envs.environment import UnityEnvironment as UE
 
 if __name__ == "__main__":
     env = UE(file_name='AIUnityProject', seed=1, side_channels=[])
+    
+    # Learning Parameters
+    MAX_EPISODES = 10
+    MAX_STEPS    = 2000
 
-    state = env.reset()
-    print(state)
+    # Environment Constants
+    env.reset()
+    TEAM1 = list(env.behavior_specs)[0]
+    TEAM1_SPEC = env.behavior_specs[TEAM1]
+
+    for episode in range(MAX_EPISODES):
+        env.reset()
+        decision_steps, terminal_steps = env.get_steps(TEAM1)
+        done = False
+        episode_rewards = 0 # Rewards of the tracked_agent (US)
+        step = 0
+        while not done and step < MAX_STEPS:
+            #print("Current Episode: ", episode, " Current Step: ", step)
+
+            # Grab our agent
+            tracked_agent = decision_steps.agent_id[0]
+            # Determine actions for each team
+            team1_action = TEAM1_SPEC.action_spec.random_action(len(decision_steps))
+            # Set actions
+            env.set_actions(TEAM1, team1_action)
+            # Move simulation forward
+            env.step()
+            # Check new simulation
+            decision_steps, terminal_steps = env.get_steps(TEAM1)
+            if tracked_agent in decision_steps: # The agent request a decision
+                episode_rewards += decision_steps[tracked_agent].reward
+            if tracked_agent in terminal_steps: # The game has reached a terminal state
+                episode_rewards += terminal_steps[tracked_agent].reward
+                done = True
+                print("Our agent received a total reward of ", episode_rewards, " for episode: ", episode)
+            step += 1 
+    env.close()
+    print("Closed Environment")
--- test.py
+++ test.py
@@ -3,33 +3,22 @@ from mlagents_envs.environment import UnityEnvironment as UE
 
 env = UE(file_name='AIUnityProject', seed=1, side_channels=[])
 
-env.reset()
+print("Number of teams: ", len(env.behavior_specs))
 
-behavior_name = list(env.behavior_specs)[0]
-print(f"Name of the behavior : {behavior_name}")
-spec = env.behavior_specs[behavior_name]
-print(spec)
-
-
-print("Number of observations : ", len(spec.observation_specs))
-
-if spec.action_spec.is_continuous():
-  print("The action is continuous")
-
-if spec.action_spec.is_discrete():
-  print("The action is discrete")
-
-decision_steps, terminal_steps = env.get_steps(behavior_name)
-print(decision_steps.obs)
+team1 = list(env.behavior_specs)[0]
+team1_spec = env.behavior_specs[team1]
 
 
 for episode in range(3):
   env.reset()
-  decision_steps, terminal_steps = env.get_steps(behavior_name)
+  decision_steps, terminal_steps = env.get_steps(team1)
   tracked_agent = -1 # -1 indicates not yet tracking
   done = False # For the tracked_agent
   episode_rewards = 0 # For the tracked_agent
-  while not done:
+  max_steps = 20
+  steps = 0
+  while not done and steps < max_steps:
+    steps += 1
     # Track the first agent we see if not tracking
     # Note : len(decision_steps) = [number of agents that requested a decision]
     if tracked_agent == -1 and len(decision_steps) >= 1:
@@ -38,19 +27,15 @@ for episode in range(3):
     '''This is where we will implement an algorithm for learning, currently this is just random learning'''
     action = spec.action_spec.random_action(len(decision_steps))
     # Set the actions
-    env.set_actions(behavior_name, action)
+    env.set_actions(team1, action)
     # Move the simulation forward
     env.step()
     # Get the new simulation results
-    decision_steps, terminal_steps = env.get_steps(behavior_name)
+    decision_steps, terminal_steps = env.get_steps(team1)
     if tracked_agent in decision_steps: # The agent requested a decision
       episode_rewards += decision_steps[tracked_agent].reward
     if tracked_agent in terminal_steps: # The agent terminated its episode
       episode_rewards += terminal_steps[tracked_agent].reward
       done = True
-  print(f"Total rewards for episode {episode} is {episode_rewards}")
-
-
-
 env.close()
 print("Closed environment")
